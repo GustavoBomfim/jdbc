1c637d44f12c421b149a3541a14509fb31538149
GustavoBomfim <gustavosoaresbomfim@hotmail.com>
2022-12-04 17:25:00 -0300

    Inserindo mysql-connector e conectando com o banco de dados

---
+++ TesteConexao.java
@@ -0,0 +1,13 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TesteConexao {
+    public static void main(String[] args) throws SQLException {
+        Connection connection = DriverManager
+                .getConnection("jdbc:mysql://localhost/loja_virtual?useTimeZone=true&serverTimeZone=UTC", "root","password");
+        System.out.println("Conexao bem sucedida");
+
+        connection.close();
+    }
+}
